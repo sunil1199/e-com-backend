c93812be7341f7feb51c8229c769c464fb6f4075
sunil1199 <sunilp.frontendarmy@gmail.com>
2023-09-28 17:37:55 +0530

    [server.js changes]

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['myKeyA', 'myKeyB'],
+    keys: ['toBeModified1', 'toBeModified2'],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
